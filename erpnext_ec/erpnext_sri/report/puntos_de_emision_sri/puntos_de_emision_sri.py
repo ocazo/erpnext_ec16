148b952ebd002d9b3dfcfe0dd9ff5e0db3af91e7
BeebTech <ronald.chonillo@gmail.com>
2026-09-15 02:21:02 +0000

    feat(sri): Puntos de Emision report and workspace shortcut
    The Puntos de Emision shortcut pointed to a child DocType (404). Added a
    Script Report listing establishment/emission points and linked the shortcut
    to it.

---
+++ erpnext_ec/erpnext_sri/report/puntos_de_emision_sri/puntos_de_emision_sri.py
@@ -0,0 +1,51 @@
+import frappe
+
+
+def execute(filters=None):
+	columns = [
+		{
+			"label": "Establecimiento",
+			"fieldname": "parent",
+			"fieldtype": "Link",
+			"options": "Sri Establishment",
+			"width": 180,
+		},
+		{"label": "Punto de Emisión", "fieldname": "record_name", "fieldtype": "Data", "width": 120},
+		{"label": "Descripción", "fieldname": "description", "fieldtype": "Data", "width": 220},
+		{
+			"label": "Ambiente",
+			"fieldname": "sri_environment_lnk",
+			"fieldtype": "Link",
+			"options": "Sri Environment",
+			"width": 120,
+		},
+		{"label": "Sec. Factura", "fieldname": "sec_factura", "fieldtype": "Int", "width": 110},
+		{"label": "Sec. Nota Crédito", "fieldname": "sec_notacredito", "fieldtype": "Int", "width": 130},
+		{"label": "Sec. Nota Débito", "fieldname": "sec_notadebito", "fieldtype": "Int", "width": 130},
+		{
+			"label": "Sec. Retención",
+			"fieldname": "sec_comprobanteretencion",
+			"fieldtype": "Int",
+			"width": 130,
+		},
+		{
+			"label": "Sec. Liquidación",
+			"fieldname": "sec_liquidacioncompra",
+			"fieldtype": "Int",
+			"width": 130,
+		},
+		{"label": "Sec. Guía", "fieldname": "sec_guiaremision", "fieldtype": "Int", "width": 110},
+	]
+
+	data = frappe.db.sql(
+		"""
+		SELECT parent, record_name, description, sri_environment_lnk,
+			sec_factura, sec_notacredito, sec_notadebito,
+			sec_comprobanteretencion, sec_liquidacioncompra, sec_guiaremision
+		FROM `tabSri Ptoemi`
+		ORDER BY parent, record_name
+		""",
+		as_dict=True,
+	)
+
+	return columns, data
